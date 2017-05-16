267cac660857737ed49f8360801b22dc5ba3bcad
Anh Vũ Đỗ <anhvudo@gmail.com>
2017-05-16 12:01:56 +0700

    Factory code with coding standards

--- src/Jackiedo/LogReader/LogReaderServiceProvider.php
+++ src/Jackiedo/LogReader/LogReaderServiceProvider.php
@@ -2,46 +2,45 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class LogReaderServiceProvider extends ServiceProvider {
+class LogReaderServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('jackiedo/log-reader');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('jackiedo/log-reader');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['log-reader'] = $this->app->share(function($app)
-		{
-			return new LogReader;
-		});
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['log-reader'] = $this->app->share(function ($app) {
+            return new LogReader;
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return ['log-reader'];
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['log-reader'];
+    }
 }
